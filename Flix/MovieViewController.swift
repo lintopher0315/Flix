c98fd1c34c8404b1c3fd319a198874cd9579092f
Christopher Lin <christopherlin@Christophers-iMac.local>
2019-01-20 12:30:37 -0700

    added view

---
+++ Flix/MovieViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieViewController.swift
+//  Flix
+//
+//  Created by Christopher Lin on 1/20/19.
+//  Copyright © 2019 Christopher Lin. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
