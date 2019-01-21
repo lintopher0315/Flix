42e723221aa402aed3e2a8f62c6cb98cbe4d1d1f
Christopher Lin <christopherlin@Christophers-iMac.local>
2019-01-21 13:29:30 -0700

    added network request snippet

--- Flix/MovieViewController.swift
+++ Flix/MovieViewController.swift
@@ -9,11 +9,31 @@
 import UIKit
 
 class MovieViewController: UIViewController {
+    
+    var movies = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                // TODO: Get the array of movies
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
